35ab95b95d3c306c20f571fb6bb0342c987da0c5
Arenukvern <anton@xsoulspace.dev>
2023-12-05 02:42:56 +0200

    fix: bottstrap

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -8,24 +8,25 @@ import 'package:xsoulspace/pack_core/global_states/global_states.dart';
 Future<void> bootstrap(
   final Widget Function({
     required AppServicesProviderDiDto servicesDiDto,
-  })
-      builder,
+  }) builder,
 ) async {
-  WidgetsFlutterBinding.ensureInitialized();
   initWebView();
   final firebaseIntializer = GlobalStateNotifiers.getFirebaseIntializer();
   await firebaseIntializer.onLoad(DefaultFirebaseOptions.currentPlatform);
 
-  // await Flame.device.fullScreen();
   final analyticsNotifier = GlobalStateNotifiers.getAnalytics();
-  final userNotifier = GlobalStateNotifiers.getUser();
-  await analyticsNotifier.onLoad();
-  final servicesDiDto = AppServicesProviderDiDto(
-    analyticsNotifier: analyticsNotifier,
-    userNotifier: userNotifier,
-  );
-  runZonedGuarded(
-    () => runApp(builder(servicesDiDto: servicesDiDto)),
+
+  return runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      final userNotifier = GlobalStateNotifiers.getUser();
+      await analyticsNotifier.onLoad();
+      final servicesDiDto = AppServicesProviderDiDto(
+        analyticsNotifier: analyticsNotifier,
+        userNotifier: userNotifier,
+      );
+      runApp(builder(servicesDiDto: servicesDiDto));
+    },
     analyticsNotifier.recordError,
   );
 }
